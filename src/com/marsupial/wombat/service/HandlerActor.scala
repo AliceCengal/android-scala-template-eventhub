28584b3502d883967d56112e162bea38825cebd2
AliceCengal <cengallut@gmail.com>
2014-05-10 12:02:26 -0500

    Removed the old system

--- src/com/marsupial/wombat/service/HandlerActor.scala
+++ src/com/marsupial/wombat/service/HandlerActor.scala
@@ -1,38 +1,18 @@
 package com.marsupial.wombat.service
 
-import android.os.{Looper, Message, Handler}
+import android.os.{Message, Handler}
 
 /**
  * A basic Actor that uses Android's messaging framework.
  */
-trait HandlerActor extends Handler with Actor {
-
-  override def !(msg: AnyRef): Unit = {
-    Message.obtain(this, 0, msg).sendToTarget()
-  }
-  
-  override def tell(msg: AnyRef): Unit = {
-    Message.obtain(this, 0, msg).sendToTarget()
-  }
-
-  override def ?(msg: AnyRef)(implicit requester: HandlerActor): Unit = {
-    Message.obtain(this, 0, (requester, msg)).sendToTarget()
-  }
-
-  override def request(msg: AnyRef)(implicit requester: HandlerActor): Unit = {
-    Message.obtain(this, 0, (requester, msg)).sendToTarget()
-  }
-  
-}
-
-trait Actor {
+trait HandlerActor {
   def !(msg: AnyRef): Unit
-  def ?(msg: AnyRef)(implicit requester: HandlerActor): Unit
+  def ?(msg: AnyRef)(implicit requester: Handler): Unit
   def tell(msg: AnyRef): Unit
-  def request(msg: AnyRef)(implicit requester: HandlerActor): Unit
+  def request(msg: AnyRef)(implicit requester: Handler): Unit
 }
 
-class ActorShell(h: Handler) extends Actor {
+class ActorShell(h: Handler) extends HandlerActor {
   override def !(msg: AnyRef): Unit = {
     Message.obtain(h, 0, msg).sendToTarget()
   }
@@ -41,44 +21,15 @@ class ActorShell(h: Handler) extends Actor {
     Message.obtain(h, 0, msg).sendToTarget()
   }
 
-  override def ?(msg: AnyRef)(implicit requester: HandlerActor): Unit = {
+  override def ?(msg: AnyRef)(implicit requester: Handler): Unit = {
     Message.obtain(h, 0, (requester, msg)).sendToTarget()
   }
 
-  override def request(msg: AnyRef)(implicit requester: HandlerActor): Unit = {
+  override def request(msg: AnyRef)(implicit requester: Handler): Unit = {
     Message.obtain(h, 0, (requester, msg)).sendToTarget()
   }
 }
 
 trait ActorConversion {
-  implicit def handlerToActor(h: Handler): Actor = new ActorShell(h)
-}
-
-object HandlerActor {
-
-  /**
-   * Create a synchronous Handler that runs tasks on the main thread
-   */
-  def sync(callback: Handler.Callback): HandlerActor = {
-    new SyncHandlerActor(callback)
-  }
-
-  /**
-   * Create an asynchronous Handler that runs tasks on a separate HandlerThread,
-   * one whose looper is passed in as parameter.
-   */
-  def async(looper: Looper,
-            callback: Handler.Callback): HandlerActor = {
-    new AsyncHandlerActor(looper, callback)
-  }
-
-  private class AsyncHandlerActor(looper: Looper,
-                                  callback: Handler.Callback)
-      extends Handler(looper, callback)
-              with HandlerActor
-
-  private class SyncHandlerActor(callback: Handler.Callback)
-      extends Handler(callback)
-              with HandlerActor
-
+  implicit def handlerToActor(h: Handler): HandlerActor = new ActorShell(h)
 }
